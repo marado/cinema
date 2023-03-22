db4ef33bc4f1dba1cb5954c8d9021548f809bf89
Marcos Marado <mindboosternoori@gmail.com>
2023-03-22 20:07:46 +0000

    remove the null character we're previously adding
    In order to separate unicode characters we're adding a null character
    after it, but once the string is actually printed, we need to strip
    out the null characters again.

--- cinema.sh
+++ cinema.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Filmes em Exibicao:"
 echo ""
-while read -r line; do printf "$line\n"; done < <(wget http://www.cinecartaz.publico.pt -o /dev/null -O - |grep value=\"/filme/|cut -d\> -f2-|cut -d\< -f1|sed "s/\&#x/\\\U/g"|sed "s/;/\\\0/g"| sort -u)
+while read -r line; do printf "$line\n"|sed 's/\x0//g'; done < <(wget http://www.cinecartaz.publico.pt -o /dev/null -O - |grep value=\"/filme/|cut -d\> -f2-|cut -d\< -f1|sed "s/\&#x/\\\U/g"|sed "s/;/\\\0/g"| sort -u)
